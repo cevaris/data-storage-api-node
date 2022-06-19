4d7e3ea585af761977a84efb1e32259744c014f8
Adam Cardenas <cevaris@gmail.com>
2022-06-18 20:07:28 -0600

    added specific repo validation tests

--- tests/common/hashing.test.ts
+++ tests/common/hashing.test.ts
@@ -1,6 +1,6 @@
 import { toSha256Hex } from "../../src/common/hashing";
 
-describe('toSha256Hex()', () => {
+describe('common - hashing', () => {
     test('encrypts string correclty and consistently', () => {
         const expectedHash = 'f5903f51e341a783e69ffc2d9b335048716f5f040a782a2764cd4e728b0f74d9';
         expect(toSha256Hex('apples')).toBe(expectedHash);
---
+++ tests/storage/repository.test.ts
@@ -0,0 +1,44 @@
+import { MAX_REPOSITORY_LENGTH } from "../../src/common/config";
+import { InvalidRepositoryName } from "../../src/common/errors";
+import { PersistedRepositoryObject } from "../../src/storage/persisted";
+import { persistedRepositoryObjectToKey, toKey, validateRepositoryName } from "../../src/storage/repository";
+
+describe('storage - repository', () => {
+    test('valid repository name', () => {
+        // if these tests dont throw, then they are success full
+        expect(validateRepositoryName('thisIsAValidName123')).toBe(undefined);
+        expect(validateRepositoryName('valid.name_1-')).toBe(undefined);
+    });
+
+    test('invalid repository name - invalid chars', () => {
+        expect(() => validateRepositoryName('!@#$'))
+            .toThrowError(new InvalidRepositoryName(`Repository contains invalid characters.`));
+        expect(() => validateRepositoryName('.'))
+            .toThrowError(new InvalidRepositoryName(`Repository contains invalid characters.`));
+    });
+
+    test('invalid repository name - too long', () => {
+        expect(() => validateRepositoryName('a'.repeat(MAX_REPOSITORY_LENGTH + 1)))
+            .toThrowError(new InvalidRepositoryName(`Repository name length must be less than ${MAX_REPOSITORY_LENGTH}.`));
+    });
+
+    test('invalid repository name - empty string', () => {
+        expect(() => validateRepositoryName(''))
+            .toThrowError(new InvalidRepositoryName(`Repository name must be non-empty.`));
+    });
+
+    test('construct repository objectId key', () => {
+        expect(toKey('repo', 'oid')).toBe('repo/oid');
+    });
+
+    test('construct PersistedRepositoryObject key', () => {
+        const persistedRepositoryObject: PersistedRepositoryObject = {
+            createdAt: new Date(),
+            repository: 'a',
+            oid: 'b',
+            blob: 'blob',
+            size: 4,
+        };
+        expect(persistedRepositoryObjectToKey(persistedRepositoryObject)).toBe('a/b');
+    });
+});
